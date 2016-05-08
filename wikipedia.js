7cb52f6e3e746243446e45b919520c2f32cb6276
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2016-05-08 12:25:59 +1000

    Create wikipedia.js

---
+++ wikipedia.js
@@ -0,0 +1,12 @@
+// makeKnowledgeFunky
+function makeKnowledgeFunky(ev){ 
+	ev.preventDefault(); 
+	var el = $(ev.target); 
+	$.get(el.attr('href'), function(res){ 
+		var paragraph = $('#mw-content-text p', res).first(); 
+		var content = $("<div style='padding:1em;border:1px solid #ffe;'>"+paragraph.html()+"</div>");
+		$('a', content).click(makeKnowledgeFunky);
+		el.after(content);
+	});
+};
+$('a').click(makeKnowledgeFunky)
